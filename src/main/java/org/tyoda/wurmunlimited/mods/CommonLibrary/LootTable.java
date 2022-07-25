2cfa150fe9de5ff515aeb45ec491f3c6f90765ff
Tyoda <markus.matyas002@gmail.com>
2022-07-25 16:28:20 +0200

    update

--- src/main/java/org/tyoda/wurmunlimited/mods/CommonLibrary/LootTable.java
+++ src/main/java/org/tyoda/wurmunlimited/mods/CommonLibrary/LootTable.java
@@ -1,18 +1,38 @@
 package org.tyoda.wurmunlimited.mods.CommonLibrary;
 
+import com.wurmonline.server.FailedException;
+import com.wurmonline.server.items.Item;
+import com.wurmonline.server.items.ItemFactory;
+import com.wurmonline.server.items.NoSuchTemplateException;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Random;
 
 public class LootTable {
     public static final Random random = CommonLibrary.random;
+    public static final int rareChance = 10000;
+    public static final int supremeChance = 100000;
+    public static final int fantasticChance = 1000000;
     public final HashMap<Integer, ArrayList<Integer>> lootTable = new HashMap<>();
+
+    /**
+     * Initializes the lootTable.
+     * @param newLootTable A Hashmap that maps the weight of the groups to the Template IDs
+     *                     of the items they contain.
+     */
     public LootTable(HashMap<Integer, ArrayList<Integer>> newLootTable){
         for (Integer i : newLootTable.keySet()) {
             ArrayList<Integer> arr = new ArrayList<>(newLootTable.get(i));
             this.lootTable.put(i, arr);
         }
     }
+    /**
+     * Randomly generates a list of items from the loottable.
+     * @param maxWeight This is the maximum amount of weight the items will be worth.
+     * @return An ArrayList containing the Template IDs of the generated items.
+     */
     public ArrayList<Integer> getLoot(int maxWeight){
         ArrayList<Integer> loot = new ArrayList<>();
         int lootValue = 0;
@@ -30,6 +50,12 @@ public class LootTable {
         }
         return loot;
     }
+
+    /**
+     * Returns the appropriate types of coins for a given value.
+     * @param moneyPool The value of the coins to be generated.
+     * @return An ArrayList containing the templateIds of the generated coins.
+     */
     public static ArrayList<Integer> generateCoins(int moneyPool){
         int currentItemTemplate = 61; // gold-twenty coins
         int currentSteps = 20000000;
@@ -87,5 +113,66 @@ public class LootTable {
             moneyPool -= steps;
         }
         return coins;
+    }
+
+    /**
+     * Returns a random quality.
+     * @return A float between 0 and 100;
+     */
+    private static float randomQuality(){
+        return random.nextInt(100)+random.nextFloat();
+    }
+
+    /**
+     * Returns a random rarity.
+     * @return A random byte between 0 and 3 (inclusive)
+     */
+    private static byte randomRarity(){
+        return randomRarity(rareChance, supremeChance, fantasticChance);
+    }
+    /**
+     * Returns a random rarity.
+     * @param rare The chance of 1 in rare of any of the items being Rare
+     * @param supreme The chance of 1 in supreme of any of the items being Supreme
+     * @param fantastic The chance of 1 in fantastic of any of the items being Fantastic
+     * @return A random byte between 0 and 3 (inclusive)
+     */
+    private static byte randomRarity(int rare, int supreme, int fantastic){
+        byte rarity = 0;
+        if(random.nextInt(rare) == 0) rarity = 1;
+        if(random.nextInt(supreme) == 0) rarity = 2;
+        if(random.nextInt(fantastic) == 0) rarity = 3;
+        return rarity;
+    }
+    /**
+     * Generates items of random quality and rarity from the supplied Template IDs
+     * Rare chance is 1 in 10.000, Supreme chance is 1 in 100.000
+     * Fantastic chance is 1 in 1.000.000
+     * @param templates Array of Template IDs to generate items from
+     * @return ArrayList<Item> containing a list of the items generated
+     */
+    public static ArrayList<Item> createItems(ArrayList<Integer> templates)
+            throws NoSuchTemplateException, FailedException {
+        return createItems(templates, rareChance, supremeChance, fantasticChance);
+    }
+    /**
+     * Generates items of random quality and rarity from the supplied Template IDs
+     * @param templates Array of Template IDs to generate items from
+     * @param rare The chance of 1 in rare of any of the items being Rare
+     * @param supreme The chance of 1 in supreme of any of the items being Supreme
+     * @param fantastic The chance of 1 in fantastic of any of the items being Fantastic
+     * @return ArrayList<Item> containing a list of the items generated
+     */
+    public static ArrayList<Item> createItems(ArrayList<Integer> templates, int rare, int supreme, int fantastic)
+            throws NoSuchTemplateException, FailedException {
+        ArrayList<Item> items = new ArrayList<>();
+
+        for(int currentTemplateId : templates) {
+            items.add(ItemFactory.createItem(currentTemplateId,
+                    randomQuality(), randomRarity(rare, supreme, fantastic), null));
+        }
+
+        Collections.shuffle(items);
+        return items;
     }
 }
--- src/main/java/org/tyoda/wurmunlimited/mods/CommonLibrary/WorldPosition2D.java
+++ src/main/java/org/tyoda/wurmunlimited/mods/CommonLibrary/WorldPosition2D.java
@@ -16,19 +16,43 @@ public class WorldPosition2D {
     private int tileY = 0;
     private float meterX = 0;
     private float meterY = 0;
+
+    /**
+     * Initializes a WorldPosition2D to (0, 0).
+     */
     public WorldPosition2D(){}
+    /**
+     * Initializes a WorldPosition2D for the given tile position.
+     * @param x The x value of the tile position.
+     * @param y The y value of the tile position.
+     */
     public WorldPosition2D(float x, float y){
         setMeterPos(x, y);
     }
+    /**
+     * Initializes a WorldPosition2D for the given tile position.
+     * @param x The x value of the meter position.
+     * @param y The y value of the meter position.
+     */
     public WorldPosition2D(int x, int y){
         setTilePos(x, y);
     }
+    /**
+     * Sets the tileX, tileY to the given values, and meterX, meterY to four times their values.
+     * @param x The x value of the tile position.
+     * @param y The y value of the tile position.
+     */
     public void setTilePos(int x, int y){
         tileX = x;
         tileY = y;
         meterX = x<<2;
         meterY = x<<2;
     }
+    /**
+     * Sets the meterX, meterY to the given values, and tileX, tileY to a fourth of their values.
+     * @param x The x value of the meter position.
+     * @param y The y value of the meter position.
+     */
     public void setMeterPos(float x, float y){
         meterX = x;
         meterY = y;
@@ -38,6 +62,14 @@ public class WorldPosition2D {
     public static WorldPosition2D getRandomPos(){
         return getRandomPos(200, 500);
     }
+
+    /**
+     * Generates a random position on the server.
+     * @param padding The minimum distance from the map's borders.
+     * @param maxTries The maximum number of times it tries to generate a random position.
+     * @return A WorldPosition2D containing a random position. Returns null if no suitable
+     *         position was found.
+     */
     public static WorldPosition2D getRandomPos(int padding, int maxTries){
         // This code is mostly copied from
         // https://github.com/dmon82/TreasureHunting/blob/master/src/com/pveplands/treasurehunting/Treasuremap.java
@@ -55,17 +87,15 @@ public class WorldPosition2D {
 
             isWaterOrLava = false;
 
-            for (int x = pos.tileX-1; x <= pos.tileX + 1; ++x) {
-                for (int y = pos.tileY-1; y <= pos.tileY + 1; ++y) {
+            for (int x = pos.tileX-1; x <= (pos.tileX + 1) && !isWaterOrLava; ++x) {
+                for (int y = pos.tileY-1; y <= (pos.tileY + 1) && !isWaterOrLava; ++y) {
                     int tile = Server.surfaceMesh.getTile(x, y);
 
                     if (Terraforming.isTileUnderWater(tile, x, y, true)
                             || Tiles.decodeType(tile) == Tiles.Tile.TILE_LAVA.id) {
                         isWaterOrLava = true;
-                        break;
                     }
                 }
-                if(isWaterOrLava) break;
             }
 
             if (isWaterOrLava) { ++waterCount; continue; }
