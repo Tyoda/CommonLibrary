860d5dea76e33114160cdc9757e9d662c3887657
Tyoda <markus.matyas002@gmail.com>
2022-07-28 14:17:52 +0200

    Update to 0.0.5

--- src/main/java/org/tyoda/wurmunlimited/mods/CommonLibrary/CommonLibrary.java
+++ src/main/java/org/tyoda/wurmunlimited/mods/CommonLibrary/CommonLibrary.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 public class CommonLibrary implements WurmServerMod {
     public static final Random random = new Random();
     public static final Logger logger = Logger.getLogger(CommonLibrary.class.getName());
-    private static final String version = "0.0.3";
+    private static final String version = "0.0.5";
     @Override
     public String getVersion() {
         return version;
